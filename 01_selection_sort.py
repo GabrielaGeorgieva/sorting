17a8f7e6cd39568d19d9c5aba6e415f7d86521e1
gabriela1604 <gabriela.georgieva1604@gmail.com>
2017-06-01 21:58:07 +0200

    docstrings for selection sort

--- 01_selection_sort.py
+++ 01_selection_sort.py
@@ -1,4 +1,9 @@
 def selection_sort(array):
+    """
+
+    :param array: python list with integers
+    :return: list: sorted array
+    """
     n = len(array)
     for i in range(0, n-1):
         for j in range(i+1, n):
@@ -7,11 +12,17 @@ def selection_sort(array):
     return array
 
 
-A = selection_sort([9, 8, 7, 3, 1, 10, 6, 5, 4, 2])
-print(A)
+# A = selection_sort([9, 8, 7, 3, 1, 10, 6, 5, 4, 2])
+# print(A)
 
 
 def selection_sort_complexity(array):
+    """
+    Understanding complexity of selection sort algorithm
+
+    :param array: python list with integers
+    :return: list: sorted array
+    """
     exchanges = 0
     comparisons = 0
     n = len(array)
@@ -21,13 +32,13 @@ def selection_sort_complexity(array):
                 array[i], array[j] = array[j], array[i]
                 exchanges += 1
             comparisons += 1
-        print("Stand nach Iteration", i, ":", array)
-    print(n, "Elemente sortiert mit", exchanges,
-          "Austauschen und ", comparisons, "Vergleichen")
+        print("Array after iteration", i, ":", array)
+    print(n, "elements sorted with", exchanges,
+          "exchanges and ", comparisons, "comparisons")
     return array
 
-A = selection_sort_complexity([9, 8, 7, 3, 1, 10, 6, 5, 4, 2])
-print(A)
+# A = selection_sort_complexity([9, 8, 7, 3, 1, 10, 6, 5, 4, 2])
+# print(A)
 
 
 def selection_sort_divisible(array):
